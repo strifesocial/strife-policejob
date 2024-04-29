bc2a34c3bc1222ef2771b108e630c27de9fb8171
Strife Social <166969842+strifesocial@users.noreply.github.com>
2024-04-29 13:58:11 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'Strife Social Developments'
+description 'Script for police officers to pull over NPC vehicles'
+version '1.0'
+
+dependencies {
+    'qb-core',
+    'qb-policejob'
+}
+
+client_script 'client.lua'
+server_script 'server.lua'
